ea7e06ec2448cdaf717ecbc68d1f327079431250
IreneWu05 <109931506+IreneWu05@users.noreply.github.com>
2022-10-25 22:24:01 +1300

    Update run.py

--- run.py
+++ run.py
@@ -17,4 +17,34 @@ def index():
     return render_template('index.html', datas=res.fetchall())
 
     db.close()
+    
+    ########test
+    
+        from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+ACTIVEDATADB = 'activedata.db'
+
+#import database
+import sqlite3
+
+
+def fetchactivedata(con):
+
+    datas = []
+    cur = con.execute('SELECT Value from data WHERE ID = 739')
+    for row in cur:
+        datas.append(list(row))
+
+    return{'Value':datas}
+
+@app.route('/')
+def index():
+    con = sqlite3.connect(ACTIVEDATADB)
+    activedata = fetchactivedata(con)
+    con.close()
+    return render_template('index.html', datas=activedata['Value'])
+
+
      

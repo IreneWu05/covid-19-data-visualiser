6f9c6ffabcdcd203cf111ce1ecdb992fea263f68
IreneWu05 <109931506+IreneWu05@users.noreply.github.com>
2022-10-26 14:44:57 +1300

    Update run.py

--- run.py
+++ run.py
@@ -9,42 +9,15 @@ import sqlite3
 
 @app.route('/')
 def index():
-
+    
+    Value = []
     con = sqlite3.connect('activedata.db')
-    con.row_factory = sqlite3.Row
-    db = con.cursor()
-    res = db.execute("SELECT Value from data WHERE ID = 739")
-    return render_template('index.html', datas=res.fetchall())
+    cur = con.cursor()
+    sql = "SELECT Value from data WHERE ID = 739 AND 747"
+    data1 = cur.execute(sql)
+    for item in data1:
+        Value.append(item)
 
-    db.close()
-    
-    ########test
-    
-        from flask import Flask, render_template, request
-app = Flask(__name__)
-
-
-ACTIVEDATADB = 'activedata.db'
-
-#import database
-import sqlite3
-
-
-def fetchactivedata(con):
-
-    datas = []
-    cur = con.execute('SELECT Value from data WHERE ID = 739')
-    for row in cur:
-        datas.append(list(row))
-
-    return{'Value':datas}
-
-@app.route('/')
-def index():
-    con = sqlite3.connect(ACTIVEDATADB)
-    activedata = fetchactivedata(con)
+    cur.close()
     con.close()
-    return render_template('index.html', datas=activedata['Value'])
-
-
-     
+    return render_template('index.html',Value=Value)

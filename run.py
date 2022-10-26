867dc3d8abe4fb1d64a0584a15e9e3894ca96f2d
IreneWu05 <109931506+IreneWu05@users.noreply.github.com>
2022-10-26 17:30:44 +1300

    Update run.py

--- run.py
+++ run.py
@@ -13,11 +13,12 @@ def index():
     Value = []
     con = sqlite3.connect('activedata.db')
     cur = con.cursor()
-    sql = "SELECT Value from data WHERE ID = 739 AND 747"
+    sql = "SELECT Value from data WHERE ID = 749 OR ID = 752 OR ID = 751 OR ID = 747"
     data1 = cur.execute(sql)
     for item in data1:
         Value.append(item)
-
+        
+    
     cur.close()
     con.close()
     return render_template('index.html',Value=Value)
